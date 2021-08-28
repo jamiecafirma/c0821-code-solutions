ba8a9dca78bd80f06b4858578896d3ae9d97f43f
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-28 14:56:03 -0700

    Simplified code.

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,16 +1,14 @@
 /* exported initial */
 
 // create storage for output
-// create storage for index of last element
-// subtract length of input by 1 to get last index, then add it to storage
+// create storage for index of last element, then subtract length of input by 1 to get last index
 // look at each element in input except for the last
 // - add current element to output
 // return output
 
 function initial(array) {
   var exceptLast = [];
-  var lastIndex = 0;
-  lastIndex += array.length - 1;
+  var lastIndex = array.length - 1;
   for (var i = 0; i < lastIndex; i++) {
     exceptLast.push(array[i]);
   }
--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,15 +1,13 @@
 /* exported last */
 
 // create storage for output
-// create storage for index of last element of input
-// subtract 1 from the length of the input and add it to the index storage
+// create storage for index of last element of input, then subtract 1 from the length of the input to get the last index
 // get the value of the last element using the index and add it to the output
 // return output
 
 function last(array) {
   var lastElement;
-  var lastIndex = 0;
-  lastIndex += array.length - 1;
+  var lastIndex = array.length - 1;
   lastElement = array[lastIndex];
   return lastElement;
 }
