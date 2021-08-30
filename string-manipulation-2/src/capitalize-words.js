cf09df52a2df95a4509c2a7714c7a1acfeac89d6
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-30 11:29:47 -0700

    added return to capitalizeWord function.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -3,36 +3,40 @@
 // create storage for output
 // create storage for current word
 // create a function to capitalize a word
-// - make the first character of current word uppercase and add it to the output
+// - create storage for the capitalized word
+// - make the first character of current word uppercase and add it to the capitalized word
 // - look at the rest of the characters in current word
-//   - make current character lowercase and add it to the output
+//   - make current character lowercase and add it to the capitalized word
+// - return the capitalized word
 // look at each character in input
 // - if current character is a space
 //  - capitalize current word
 //  - add a space to the output
 //  - clear current word
 // - otherwise, add current character to current word
-// capitalize the current word
+// capitalize the current word and add the result to the output
 // return output
 
 function capitalizeWords(string) {
   var output = '';
   var currentWord = '';
   function capitalizeWord(word) {
-    output += word[0].toUpperCase();
+    var capitalized = '';
+    capitalized += word[0].toUpperCase();
     for (var i = 1; i < word.length; i++) {
-      output += word[i].toLowerCase();
+      capitalized += word[i].toLowerCase();
     }
+    return capitalized;
   }
   for (var i = 0; i < string.length; i++) {
     if (string[i] === ' ') {
-      capitalizeWord(currentWord);
+      output += capitalizeWord(currentWord);
       output += ' ';
       currentWord = '';
     } else {
       currentWord += string[i];
     }
   }
-  capitalizeWord(currentWord);
+  output += capitalizeWord(currentWord);
   return output;
 }
