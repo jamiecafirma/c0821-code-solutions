02cdd67f03f863d1dba13535cdf0cc63c713df37
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-30 10:46:52 -0700

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,24 @@
+console.log('hello, world');
+
+var $title = document.querySelector('h1');
+
+console.log('value of $title:', $title);
+console.dir($title);
+
+var $explanation = document.querySelector('#explanation');
+
+console.log('value of $explanation:', $explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+
+console.log('value of $hint:', $hint);
+console.dir($hint);
+
+var $paragraphs = document.querySelectorAll('p');
+
+console.log('value of $paragraphs:', $paragraphs);
+
+var $links = document.querySelectorAll('.example-link');
+
+console.log('value of $links:', $links);
