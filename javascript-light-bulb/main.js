71db0f16d0aaa80f392ec7edfb7c37d0f5c77b72
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-31 22:35:03 -0700

    used booleans to check if light bulb is on or off instead of DOM

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,19 +1,23 @@
-// create storage for light bulb object
-// create storage for the background object
+// check if the light bulb is on
 // - if the light bulb is on
 //  - turn the light bulb off and make the background dark to match
+//  - say that the light bulb is off now
 // - if it's off, turn the light bulb on and light up the background too
+//  - say that the light bulb is on now
 
+var lightOn = true;
 var $lightBulb = document.querySelector('.light-bulb');
 var $container = document.querySelector('.container');
 
 function toggleSwitch(event) {
-  if ($lightBulb.className === 'light-bulb on-switch') {
+  if (lightOn === true) {
     $container.className = 'container off-switch';
     $lightBulb.className = 'light-bulb off-switch';
+    lightOn = false;
   } else {
     $container.className = 'container on-switch';
     $lightBulb.className = 'light-bulb on-switch';
+    lightOn = true;
   }
 }
 
