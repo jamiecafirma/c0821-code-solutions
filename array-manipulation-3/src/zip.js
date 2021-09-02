c34c9ba7a3d32d1f50ef03ab374de854f8b350ef
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-09-02 16:56:34 -0700

    in progress

---
+++ array-manipulation-3/src/zip.js
@@ -0,0 +1,8 @@
+/* exported zip */
+
+// create storage for the output
+// create storage for the current array
+// if the length of the first array is longer than the second
+// - look at each item in the first array
+//  - add the current item from the first array to the current array
+//  - add the item at the current index, from the second array, to the current array
