d0b72376cf6f0a8f7bb17da472453114a649eb8b
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-09-01 23:45:24 -0700

    finished challenge but will make an alternate version of anagram.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -11,25 +11,25 @@
 //   - make the current character uppercase and add it to the output
 //  - otherwise, add the current character in lowercase to the output
 // - return the output
-// look at each character in the title
+// capitalize the first character of the title and add it to the output
+// look at each character
 // - if the current character is a space
 //  - if the current word lowercase matches javascript
-//    - add JavaScript to the output
+//    - add JavaScript to the output and current character
 //    - clear the current word
 //  - if the current word lowercase matches api
-//    - add API to the output
+//    - add API to the output and current character
 //    - clear the current word
-//  - if the last character in the output is a colon
-//   - capitalize the current word, add a space, then the word to the output, then another space
+//  - if the second to last character in the output is a colon
+//   - capitalize the current word, add the word to the output, then current character
 //   - clear the current word
-//  - if the current word is more than four characters long
-//   - capitalize the current word, then add the word and a space to the output
+//  - if the current word is four characters or more
+//   - capitalize the current word, then add the word and current character to the output
 //   - clear the current word
 //  - if the current word lowercase matches any word in the exceptions list
-//   - add the word, in lowercase, to the output, then a space
+//   - add the word, in lowercase, to the output, then current character
 //   - clear the current word
 // - otherwise, add the current character to the current word
-// add the current word to the output
 // return the output
 
 function titleCase(title) {
@@ -39,10 +39,63 @@ function titleCase(title) {
 
   function capitalize(word) {
     var capitalizedWord = '';
+
     capitalizedWord += word[0].toUpperCase();
-    for (var i = 1; i < title.length; i++) {
-      capitalizedWord += word[i].toLowerCase();
+    for (var i = 1; i < word.length; i++) {
+      if (capitalizedWord[capitalizedWord.length - 1] === '-') {
+        capitalizedWord += word[i].toUpperCase();
+      } else {
+        capitalizedWord += word[i].toLowerCase();
+      }
     }
     return capitalizedWord;
   }
+
+  for (var char = 0; char < title.length; char++) {
+    if (title[char] === ' ') {
+      if (currentWord.toLowerCase() === 'javascript') {
+        apaTitle += 'JavaScript' + title[char];
+        currentWord = '';
+      } else if (currentWord.toLowerCase() === 'api') {
+        apaTitle += 'API' + title[char];
+        currentWord = '';
+      } else if (currentWord.toLowerCase() === 'javascript:') {
+        apaTitle += 'JavaScript:' + title[char];
+        currentWord = '';
+      } else if (currentWord.toLowerCase() === 'api:') {
+        apaTitle += 'API:' + title[char];
+        currentWord = '';
+      } else if (apaTitle === '') {
+        apaTitle += capitalize(currentWord) + title[char];
+        currentWord = '';
+      } else if (apaTitle[apaTitle.length - 2] === ':') {
+        apaTitle += capitalize(currentWord) + title[char];
+        currentWord = '';
+      } else if (currentWord.length >= 4) {
+        apaTitle += capitalize(currentWord) + title[char];
+        currentWord = '';
+      } else if (currentWord.length < 4) {
+        for (var e = 0; e < exceptions.length; e++) {
+          if (currentWord.toLowerCase() === exceptions[e]) {
+            apaTitle += currentWord.toLowerCase() + title[char];
+            currentWord = '';
+          }
+        }
+        if (currentWord !== '') {
+          apaTitle += capitalize(currentWord) + title[char];
+          currentWord = '';
+        }
+      }
+    } else {
+      currentWord += title[char];
+    }
+  }
+  if (currentWord.toLowerCase() === 'javascript') {
+    apaTitle += 'JavaScript';
+  } else if (currentWord.toLowerCase() === 'api') {
+    apaTitle += 'API';
+  } else {
+    apaTitle += capitalize(currentWord);
+  }
+  return apaTitle;
 }
