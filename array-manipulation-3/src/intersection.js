0d985d66ebcf385a69dbdf40ce2329de2abc0b8e
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-09-02 21:26:11 -0700

    Completed array-manipulation-3.

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,30 @@
 /* exported intersection */
+
+// create storage for output
+// create storage for a test to check if an element is also in another array
+// look at each element in the first array
+// - look at each element in the second array
+//  - if the current element is also in the second array
+//    - set the 'is in another array' test to true
+// - if the 'is in another array' test is true
+//  - add the current element to the output
+// - reset 'is in another array' test
+// return the output
+
+function intersection(first, second) {
+  var inBothArr = [];
+  var repeated = false;
+
+  for (var i = 0; i < first.length; i++) {
+    for (var s = 0; s < second.length; s++) {
+      if (first[i] === second[s]) {
+        repeated = true;
+      }
+    }
+    if (repeated === true) {
+      inBothArr.push(first[i]);
+    }
+    repeated = false;
+  }
+  return inBothArr;
+}
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,30 @@
 /* exported unique */
+
+// create storage for the output
+// create storage for a test that keeps track of whether a value is repeated and set it to false
+// look at each element in the array
+// - look at each element in the output
+//  - if the current element in the array is found in the output
+//    - set the repeated test to true
+// - if the current element hasn't been repeated
+//  - add the current element to the output
+// - reset the repeated test
+// return the output
+
+function unique(array) {
+  var isUnique = [];
+  var repeated = false;
+
+  for (var i = 0; i < array.length; i++) {
+    for (var u = 0; u < isUnique.length; u++) {
+      if (array[i] === isUnique[u]) {
+        repeated = true;
+      }
+    }
+    if (repeated === false) {
+      isUnique.push(array[i]);
+    }
+    repeated = false;
+  }
+  return isUnique;
+}
