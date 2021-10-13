967cfa7c8654d0bd204da7be4d4a2b92707c92cd
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-10-13 19:55:11 +0000

    Added rental count to table

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -2,7 +2,8 @@ with "cte_profit" as (
   select "films"."filmId",
          "films"."replacementCost" as "cost",
          sum("payments"."amount") as "revenue",
-         sum("payments"."amount") - "films"."replacementCost" as "profit"
+         sum("payments"."amount") - "films"."replacementCost" as "profit",
+         count("payments"."amount") as "rentals"
       from "films"
       join "inventory" using ("filmId")
       join "rentals" using ("inventoryId")
@@ -12,7 +13,8 @@ with "cte_profit" as (
 select "films"."title" as "film",
        "cte_profit"."profit",
        "cte_profit"."cost",
-       "cte_profit"."revenue"
+       "cte_profit"."revenue",
+       "cte_profit"."rentals"
     from "films"
     join "cte_profit" using ("filmId")
   order by "profit" desc
