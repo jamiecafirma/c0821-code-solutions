121a4e9940d6dff615a570571f935369a538d4d5
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-10-27 00:45:38 +0000

    Completed react-accordion-component challenge.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function topicBody(props) {
+function TopicBody(props) {
   if (!props.display || props.topic !== props.currentTopic) {
+    return null;
+  } else {
     return (
-    <div className='topic-body'>
-      <p>{props.description}</p>
-    </div>
+      <div className='topic-body'>
+        <p>{props.description}</p>
+      </div>
     );
   }
 }
@@ -17,12 +19,20 @@ export default class Accordion extends React.Component {
       isOpen: false,
       currentTopic: '01'
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
+    const clickedBanner = event.target.closest('div');
     if (this.state.isOpen === false) {
-
       this.setState({ isOpen: true });
+      this.setState({ currentTopic: clickedBanner.id });
+    } else {
+      if (clickedBanner.id !== this.state.currentTopic) {
+        this.setState({ currentTopic: clickedBanner.id });
+      } else {
+        this.setState({ isOpen: false });
+      }
     }
   }
 
@@ -30,10 +40,10 @@ export default class Accordion extends React.Component {
     const topics = this.props.topics;
     const listItems = topics.map(topic =>
       <li key={topic.id}>
-        <div className='topic-header' onClick={this.handleClick} >
+        <div className='topic-header' id={topic.id} onClick={this.handleClick} >
           <h2>{topic.name}</h2>
         </div>
-        <topicBody display={this.state.isOpen} topic={topic.id} description={topic.description} currentTopic={this.state.currentTopic} />
+        <TopicBody display={this.state.isOpen} topic={topic.id} description={topic.description} currentTopic={this.state.currentTopic} />
       </li>
     );
     return (
